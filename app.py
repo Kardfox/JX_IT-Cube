887314220cb0677d37d055ae3602d007ca9e7b8b
Kardfox <kaleo.asd@gmail.com>
2022-11-17 22:45:04 +0200

    Сделаны первые наброски интерфейса, загружены шрифты

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import flask
 
 app = flask.Flask(
     __name__,
-    static_url_path="/server-storage",
+    static_url_path="/storage",
     static_folder="storage"
 )
 
@@ -10,6 +10,11 @@ app = flask.Flask(
 def main():
     return flask.render_template("main.html")
 
+@app.get("/about")
+def about():
+    return "It's a coool project"
+
 @app.get("/favicon.ico")
 def return_icon():
+    flask.url_for
     return app.send_static_file("favicon.ico")
